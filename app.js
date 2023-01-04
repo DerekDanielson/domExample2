f6f3aeffb07b3e710f5c1ad06cc25bd1208b6bc0
Derek <derekdanielson84@gmail.com>
2023-01-04 10:31:48 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let msg = 'Sign up to receive our newsletter for 10% off';
+
+function updateMessage() {
+    let el = document.getElementById('message');
+    el.textContent = msg;
+}
+
+updateMessage();
